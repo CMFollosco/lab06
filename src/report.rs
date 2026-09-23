a8dd6d48373fc74e5f31e2e8fb59dabf15ccfd68
leduke saenz <ledukesaenz@gmail.com>
2026-09-23 16:11:55 +0000

    style(report): use equals-sign borders

--- src/report.rs
+++ src/report.rs
@@ -1,5 +1,5 @@
 pub fn print_report(party: u32, days: u32, food: u32) {
-    println!("----- SUPPLY REPORT -----");
+    println!("===== SUPPLY REPORT =====");
     println!("Party size: {}", party);
     println!("Days:       {}", days);
     println!("Food:       {} lb", food);
